b6be973a885b96d73b6b2ec1148405cb86be0604
Rinat Sagitov <sagitov256@gmail.com>
2024-07-02 16:33:59 +0500

    feat: added run with logger

--- cmd/driver/main.go
+++ cmd/driver/main.go
@@ -2,17 +2,60 @@ package main
 
 import (
 	"driver_backend/internal/config"
+	"driver_backend/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	// Init config: cleanenv
 	cfg := config.MustLoad()
 
-	// TODO: Init logger: slog
+	// Init logger: slog
+	log := setupLogger(cfg.Env)
+	log.Info("starting driver server", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: Init storage: sqlite
 
 	// TODO: Init router: chi, "chi render"
 
 	// TODO: Run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
