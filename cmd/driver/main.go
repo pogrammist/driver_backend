2baaf569abe9d638c3fa1fcefe6fe8dab4bf9667
Rinat Sagitov <sagitov256@gmail.com>
2024-07-02 15:54:52 +0500

    create main file

---
+++ cmd/driver/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: Init config: cleanenv
+
+	// TODO: Init logger: slog
+
+	// TODO: Init storage: sqlite
+
+	// TODO: Init router: chi, "chi render"
+
+	// TODO: Run server
+}
